4ba462011a941b3780ed425c35cbd7f242da6e38
Carlos Julio Reyes Lenis <cbeff.j44@gmail.com>
2025-08-11 20:22:01 -0500

    Nuevos cambios

--- preprocess.py
+++ preprocess.py
@@ -1,41 +1,26 @@
-import numpy as np
-import pandas as pd
-import joblib
 import category_encoders as ce
 from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
-# df = load_dataset("nprak26/remote-worker-productivity")
-df = pd.read_csv('ecommerce_product_sales.csv')
-
-cols_to_encoded = ['Product Name','Category',
-                   'Customer Gender',
-                   'Payment Method']
+cols_to_encode = ['Product Name', 'Category', 'Customer Gender', 'Payment Method']
 
 class Preprocessor:
-    
     def __init__(self):
         self.cols_to_drop = ['Region', 'Sale Date', 'Sale ID']
-        self.freq_encoded = ce.CountEncoder(cols=cols_to_encoded, normalize=True)
+        self.freq_encoded = ce.CountEncoder(cols=cols_to_encode, normalize=True)
         self.scaler = StandardScaler()
     
-    def fit(self, df):
+    def fit(self, df: pd.DataFrame):
         df_features = df.drop(columns=self.cols_to_drop + ['Total Sales'], errors='ignore')
         df_encoded = self.freq_encoded.fit_transform(df_features)
-        num_cols = df_encoded.select_dtypes(include=['int64', 'float64']).columns # type: ignore
+        num_cols = df_encoded.select_dtypes(include=['int64', 'float64']).columns
         df_encoded[num_cols] = self.scaler.fit_transform(df_encoded[num_cols])
-        
         return df_encoded
     
-    def transform(self, df):
+    def transform(self, df: pd.DataFrame):
+        # Igual que fit pero sin fit
         df_features = df.drop(columns=self.cols_to_drop, errors='ignore')
         df_encoded = self.freq_encoded.transform(df_features)
         num_cols = df_encoded.select_dtypes(include=['int64', 'float64']).columns
         df_encoded[num_cols] = self.scaler.transform(df_encoded[num_cols])
-        
         return df_encoded
-    
-preprocessor = Preprocessor()
-df_train_preprocessed = preprocessor.fit(df) 
-
-
-joblib.dump(preprocessor, 'preprocessor.pkl')
